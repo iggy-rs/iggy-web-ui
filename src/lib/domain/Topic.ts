094174ea0900728a2e6ffef86c56b98ce39b0591
Rostislav Raykov <me@zbrox.com>
2024-03-01 19:36:56 +0100

    fix: change according to updated api response

--- src/lib/domain/Topic.ts
+++ src/lib/domain/Topic.ts
@@ -17,7 +17,7 @@ export function topicMapper(item: any): Topic {
     id: item.id,
     name: item.name,
     sizeBytes: item.size_bytes,
-    sizeFormatted: bytesFormatter(item.size_bytes),
+    sizeFormatted: item.size_bytes,
     messageExpiry: item.message_expiry ?? 0,
     messagesCount: item.messages_count,
     partitionsCount: item.partitions_count,
