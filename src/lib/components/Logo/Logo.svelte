8486ab3d83ebe17aa641ab549e80aed0af4ea768
Piotr <ziolko.piotr@outlook.com>
2025-02-21 21:30:11 +0100

    feat: add light mode logo

---
+++ src/lib/components/Logo/Logo.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  let className = '';
+  export { className as class };
+  const lightLogo = "/iggy-apache-lightbg.svg";
+  const darkLogo = "/iggy-apache-darkbg.svg";
+</script>
+
+<img src={lightLogo} class="{className} block dark:hidden" alt="Apache Iggy" />
+<img src={darkLogo} class="{className} hidden dark:block" alt="Apache Iggy" />
