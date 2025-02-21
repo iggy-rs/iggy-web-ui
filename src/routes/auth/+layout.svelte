5934da3394cfb0af3d656b1e81369753f4ae6138
Piotr <ziolko.piotr@outlook.com>
2025-02-21 21:30:11 +0100

    fix: move logo to static assets

--- src/routes/auth/+layout.svelte
+++ src/routes/auth/+layout.svelte
@@ -1,10 +1,9 @@
 <script>
-  import logo from '$lib/assets/logo.png';
   /** @type {{children?: import('svelte').Snippet}} */
   let { children } = $props();
 </script>
 
 <div class="min-h-screen bg-shadeL100 dark:bg-shadeD700 flex flex-col items-center pt-[10vh]">
-  <img src={logo} class="h-[100px] mb-10" alt="iggy" />
+  <img src="/logo.png" class="h-[100px] mb-10" alt="iggy" />
   {@render children?.()}
 </div>
